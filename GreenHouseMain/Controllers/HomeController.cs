3869f14af17e8df2c1e6d6158a15ce4403dd3f10
EugeniyUlko <eugeniy.ulko@softheme.com>
2015-09-03 19:42:34 +0300

    room is showed

--- GreenHouseMain/Controllers/HomeController.cs
+++ GreenHouseMain/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GreenHouseMain.Models;
 
 namespace GreenHouseMain.Controllers
 {
@@ -10,7 +11,16 @@ namespace GreenHouseMain.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var db = new Database1Entities();
+            var items = from i in db.Rooms
+                join fi in db.Reservations on i.RoomId equals fi.RoomId
+                where fi.UserId == 1
+                select i;
+            var r = new Room();
+            r.Number = items.ToList()[0].Number;
+            var c = new List<Room>();
+            c.Add(r);
+            return View(c);
         }
     }
 }
---
+++ GreenHouseMain/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenHouseMain.Models
+{
+    public class Room
+    {
+        public string Number { get; set; }
+    }
+}
